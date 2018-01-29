0e54f614e4a3d0dc39c91cbe1c566283fa603a00
Jerry <jerrylui803@gmail.com>
2018-01-29 15:03:07 -0500

    add test files

---
+++ temp/test.c
@@ -0,0 +1,15 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_DESCRIPTION("My kernel module");
+MODULE_AUTHOR("John Doe");
+MODULE_LICENSE("GPL");
+static int mymodule_init(void) {
+	printk( KERN_DEBUG "Hello world!\n" );
+	return 0;
+}
+static void mymodule_exit(void) {
+	printk( KERN_DEBUG "I'm outta here\n" );
+}
+module_init(mymodule_init);
+module_exit(mymodule_exit);
