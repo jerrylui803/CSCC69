dda950371c26e7067f4689236de03d7867671eca
Jerry <jerrylui803@gmail.com>
2018-02-07 16:25:30 -0500

    f

--- A1/interceptor.c
+++ A1/interceptor.c
@@ -363,10 +363,10 @@ asmlinkage long interceptor(struct pt_regs reg) {
  */
 asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 
-
+	return 0;
 	// check if the syscall is valid, and is not my_syscall itself (> 0), 
 	// technically the last comparison is not needed, since its just macro for 0
-	if !(syscall >= 0 && syscall <= NR_syscalls && syscall != MY_CUSTOM_SYSCALL){
+	if (!(syscall >= 0 && syscall <= NR_syscalls && syscall != MY_CUSTOM_SYSCALL)){
 		return EINVAL;
 	}
 	// start checking cmd
