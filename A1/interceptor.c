a391fd4ada55bdeab9bf92f6e4a133505353728c
Jerry <jerrylui803@gmail.com>
2018-02-07 20:55:09 -0500

    aa

--- A1/interceptor.c
+++ A1/interceptor.c
@@ -426,15 +426,11 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 		if ((pid_task(find_vpid(pid), PIDTYPE_PID) == NULL) && pid != 0) { 
 			return -EINVAL;
 		}
-		
-		// if it is not the case that (we are root and (call process isnt parent of the intercepte process or we try to monitor all process))
-
-		printk(KERN_ALERT "AAAAAAAAAAAAAAAAAAAAAAAAAaa %d    \n", current);
-		if (current_uid() != 0 && (check_pid_from_list(current->pid, pid) != 0 || pid == 0)){
-			
-			return -EPERM;
-		} 
 		printk( KERN_ALERT "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\n");
+		// if it is not the case that (we are root and (call process isnt parent of the intercepte process or we try to monitor all process))
+		if (current_uid() != 0 && ((check_pid_from_list(current->pid, pid) != 0 || pid == 0)){
+			return -EPERM;
+		}
 		// --------------------------------general checking ends---------------------------
 
 		if (cmd == REQUEST_START_MONITORING) {
