df2a7c173b68d0302cc70fe7ca837daa252191f2
Jerry <jerrylui803@gmail.com>
2018-02-07 20:10:02 -0500

    aa

--- A1/interceptor.c
+++ A1/interceptor.c
@@ -429,6 +429,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 		
 		// if it is not the case that (we are root and (call process isnt parent of the intercepte process or we try to monitor all process))
 		if (current_uid() != 0 && ((check_pid_from_list(current->pid, pid) != 0 || pid == 0))){
+			printk(KERN_ALERT "AAAAAAAAAAAAAAAAAAAAAAAAAaa\n")
 			return -EPERM;
 		} 
 		printk( KERN_ALERT "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\n");
